3b960045ca919f91193fb272f320364cd46a6c08
chase <chase.hildebrand4@gmail.com>
2022-03-07 22:21:51 -0500

    Make config-fish installer use symlinks

--- config-fish/fish/config.fish
+++ config-fish/fish/config.fish
@@ -2,7 +2,22 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set HW '/Users/Chase/Documents/school/UVA/2022-spring'
+
 alias ls='lsd'
 alias l='ls -lah'
 alias c='clear'
-alias hw='cd ~/Documents/school/UVA/2021-fall'
+alias hw="cd $HW"
+alias vfzf='nvim (fzf)'
+alias docs='cd ~/Documents'
+alias wd='whatsdue'
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
+set fish_prompt_pwd_dir_length 0
+
+#set fish_key_bindings fish_user_key_bindings
+source ~/.config/fish/functions/fish_user_keybindings.fish
+set fish_key_bindings fish_user_key_bindings
+
+fish_add_path ~/scripts
--- config-fish/install.sh
+++ config-fish/install.sh
@@ -10,6 +10,9 @@ source bashutils.sh
 path="~/.config/fish"
 abs_path="${path/#\~/$HOME}" # Substitute the ~ for the value in the $HOME variable
 
+dotfiles_path="~/dotfiles/config-fish/fish"
+abs_dotfiles_path="${dotfiles_path/#\~/$HOME}" # Substitute the ~ for the value in the $HOME variable
+
 # If the directory exists, back it up.
 if [ -d "$abs_path" ]; then
     mov "Backing up old config.fish file: $path --> ./bak"
@@ -17,5 +20,7 @@ if [ -d "$abs_path" ]; then
     mv $abs_path bak
 fi
 
-cpy "Installing fish config: ./config-fish/fish --> $path"
-cp -r config-fish/fish ~/.config/fish
+# cpy "Installing fish config: ./config-fish/fish --> $path"
+# cp -r config-fish/fish ~/.config/fish
+lnk "Installing ${YELLOW}fish${NC} config: $path --> $dotfiles_path"
+ln -vsnf $abs_dotfiles_path $abs_path
